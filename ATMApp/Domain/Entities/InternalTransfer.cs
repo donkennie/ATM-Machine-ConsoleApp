4a0465bde38468773587be62c3b0f5cfc8f6a0cb
donkennie <ajeigbekehinde160@gmail.com>
2022-09-07 01:02:04 +0100

    Create a method for internal transfer method

--- ATMApp/Domain/Entities/InternalTransfer.cs
+++ ATMApp/Domain/Entities/InternalTransfer.cs
@@ -9,7 +9,9 @@ namespace ATMApp.Domain.Entities
     public class InternalTransfer
     {
         public decimal TransferAmount { get; set; }
+
         public long ReciepeintBankAccountNumber { get; set; }
+
         public string RecipientBankAccountName { get; set; }
     }
 }
